737636776b61e08554624bcfe5a769f844e1f2fc
huangshk6 <k19951130@163.com>
2018-03-12 20:40:01 +0800

    Update bbr_centos7.sh

--- bbr_centos7.sh
+++ bbr_centos7.sh
@@ -8,8 +8,8 @@
 #
 ## Upgrade the kernel using the ELRepo RPM repository
 #  Install the ELRepo repository
-rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+rpm --import "https://www.elrepo.org/RPM-GPG-KEY-elrepo.org"
+rpm -Uvh "http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm"
 #  Install the latest kernel
 yum --enablerepo=elrepo-kernel -y install kernel-ml
 #  Set up the default grub2 boot entry
@@ -17,8 +17,8 @@ grub2-set-default 1
 #
 ## Enable BBR
 #  Modify the sysctl configuration
-echo 'net.core.default_qdisc=fq' | tee -a /etc/sysctl.conf
-echo 'net.ipv4.tcp_congestion_control=bbr' | tee -a /etc/sysctl.conf
+echo "net.core.default_qdisc=fq" | tee -a /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" | tee -a /etc/sysctl.conf
 sysctl -p
 #
 ## Reboot
