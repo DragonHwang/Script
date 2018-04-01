31196e56daefca6870c5bb24c078adf1c396aba7
huangshk6 <k19951130@163.com>
2018-04-01 17:42:55 +0800

    Add files via upload

---
+++ vagex_debian8.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Auto install Vagex
+#
+# System Required:  CentOS 7 x86_64
+#
+# Author:  Shuokang the Great
+#
+## Start
+#
+sudo apt-get update -y
+sudo apt-get install xfce4 -y
+sudo apt-get install vnc4server -y
+sudo apt-get install iceweasel -y
+sudo apt-get install psmisc -y
+#
+## Setup
+#
+sudo vnc4server
+sudo vnc4server -kill :1
+sudo sed -i '$a startxfce4 &' /root/.vnc/xstartup
+sudo chmod +x /root/.vnc/xstartup
