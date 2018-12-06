a835b1ac6d47f3a44a2bbcf087a53bdd9a666773
huangshk6 <k19951130@163.com>
2018-12-06 14:38:22 +0800

    Add files via upload

---
+++ ns3-env-centos.sh
@@ -0,0 +1,21 @@
+sudo yum install gcc-c++ python -y
+sudo yum install python-devel -y
+sudo yum install qt5-devel -y
+sudo yum install mercurial -y
+sudo yum install doxygen graphviz ImageMagick -y
+sudo yum install python-sphinx dia texlive texlive-latex -y
+sudo yum install openmpi openmpi-devel -y
+sudo yum install tcpdump wireshark -y
+sudo yum install sqlite sqlite-devel -y
+sudo yum install libxml2 libxml2-devel -y
+sudo yum install uncrustify -y
+sudo yum install boost-devel -y
+sudo yum install graphviz graphviz-devel python-setuptools-devel ipython -y
+sudo sudo easy_install pygraphviz -y
+sudo yum install goocanvas pygtk2-devel -y
+sudo rpm -ivh pygoocanvas-0.14.1-3.el6.nux.x86_64.rpm  -y
+sudo rpm -ivh pygoocanvas-devel-0.14.1-3.el6.nux.x86_64.rpm -y
+sudo yum install git -y
+sudo yum install gsl gsl-devel -y
+sudo yum install gtk2 gtk2-devel -y
+sudo yum install gdb valgrind -y
